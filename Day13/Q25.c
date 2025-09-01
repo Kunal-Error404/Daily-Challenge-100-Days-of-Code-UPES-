36317576342e2910905c9465803a482a11556342
Kunal-Error404 <kunalsaini73112@gmail.com>
2025-09-02 03:28:26 +0530

    COMPLETED

---
+++ Day13/Q25.c
@@ -0,0 +1,44 @@
+//Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+#include <stdio.h>
+int calc(int a, int b, char c);
+int main(){
+    int x,y;
+    char C;
+    printf("ENTER YOUR 1ST NUMBER:");
+    scanf("%d",&x);
+    printf("ENTER YOUR 2ND NUMBER:");
+    scanf("%d",&y);
+    printf("ENTER YOUR OPERATOR:");
+    getchar();//OK THIS IS SOMETHING NEW ALWAYS USE GET CHAR AFTER TAKING 2 INTEGER INPUTS TO CLEAR THE NEWLINE CHAR INPUT;
+    scanf("%c",&C);
+    printf("YOUR ANSWER IS : %d",calc(x,y,C));
+    return 0;
+}
+
+int calc(int a, int b, char c){
+    switch (c){
+        case '+':
+        return a+b;//return exits the loop so no need for break
+
+       
+         case '-':
+        return a-b;
+       
+         case '*':
+        return a*b;
+        
+        case '/':
+        if(b!=0){
+            return a/b;
+        }
+       
+        case '%':
+        if(b!=0){
+            return a%b;
+        }
+        break;
+
+    }
+}
+
