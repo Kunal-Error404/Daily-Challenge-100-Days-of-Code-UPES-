06904d016dccfa63cfb8bb099ea25bd609b09d15
Kunal-Error404 <kunalsaini73112@gmail.com>
2025-11-13 18:53:23 +0530

    .

--- Day50/Q100.c
+++ Day50/Q100.c
@@ -1,29 +1,39 @@
-//Print all sub-strings of a string.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){
-    char *str = NULL;
-    str = (char*)malloc(100*sizeof(char));
-    if(str == NULL){
-        printf("Error memory not allocated.");
+
+int main() {
+    char *str = (char*)malloc(100 * sizeof(char));
+    if (str == NULL) {
+        printf("Erro memory not allocated.\n");
         return -1;
     }
+
     printf("Enter the string: ");
-    fgets(str,100,stdin);
+    fgets(str, 100, stdin);
 
     int len = strlen(str);
-    if(str[len-1]=='\n'){
-        str[len-1]='\0';
-        len--;//\n was removed
+    if (str[len-1] == '\n') {
+        str[len-1] = '\0';
+        len--;
     }
-    int start = 0;
-    for(int i = start;str[i]!='\0';i++){
-        printf("%c",str[i]);
-        if(i==len){
-            start++;
-            printf(", ");
+// using 2 ptr method first 2 loops sets the 2 indices intp pointers and moves them
+int check = 1;
+    for (int start = 0; start < len; start++) { // Time comp O(n^3) similar to matrix mult 
+        for (int end = start; end < len; end++) {
+
+             if (!check) {//to print comma after every first substr
+                printf(", ");
+            }
+            check = 0;
+        
+            for (int k = start; k <= end; k++) {
+                printf("%c", str[k]);
+            }
+    
         }
     }
+
+    free(str);
     return 0;
 }
